3c98c7ec799fbe51e0a5a349f8ac1db772b9b596
AbhisekhNayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-05-05 15:57:41 +0530

    project

--- projects/script.js
+++ projects/script.js
@@ -31,62 +31,53 @@ document.addEventListener('visibilitychange',
 
 
 // fetch projects start
-function getProjects() 
-{
-    return fetch("projects.json")
-        .then(response => response.json())
-        .then(data => {
-            return data
-        });
-}
-
-
-function showProjects(projects) 
-{
+function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "index.html";
+    let projectsHTML = "";
     projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
+      projectsHTML += `
+      <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+          <div class="content">
+            <div class="tag">
+              <h3>${project.name}</h3>
+            </div>
+            <div class="desc">
+              <p>${project.desc}</p>
+              <div class="btns">
+                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    </div>`
+      </div>`
     });
     projectsContainer.innerHTML = projectsHTML;
-
+  
     var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows',
+      masonry: {
+        columnWidth: 200
+      }
     });
-
+  
     // filter items on button click
     $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
+      $('.button-group').find('.is-checked').removeClass('is-checked');
+      $(this).addClass('is-checked');
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
     });
-}
-
-getProjects().then(data => {
-    showProjects(data);
-})
+  
+    // handle image load errors
+    $('img').on('error', function () {
+      console.log('Error loading image: ' + $(this).attr('src'));
+    });
+  }
+  
 // fetch projects end
 
 // Start of Tawk.to Live Chat
