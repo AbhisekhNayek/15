d5e53f8f8a6a6cd1a1df14b9d3ba4703771dba00
AbhisekhNayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-05-05 15:44:58 +0530

    final

--- projects/script.js
+++ projects/script.js
@@ -44,7 +44,7 @@ function getProjects()
 function showProjects(projects) 
 {
     let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
+    let projectsHTML = "index.html";
     projects.forEach(project => {
         projectsHTML += `
         <div class="grid-item ${project.category}">
